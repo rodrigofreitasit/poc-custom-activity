280462d58570495ce6d7d01362a75b852023c0d7
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2020-08-26 15:54:45 -0300

    third configuration

--- customActivity.js
+++ customActivity.js
@@ -1,16 +1,15 @@
 var connection = new Postmonger.Session();
 
-
 connection.trigger('ready');
 
-connection.on('initActivity', function(data){
-    document.getElementById('configuration').value= JSON.stringfy(data,null,2);
+connection.on('initActivity', function (data) {
+    document.getElementById('configuration').value = JSON.stringfy(data, null, 2);
 
 });
 
 //save
 
-connection.on('clickedNext',function(){
-    var configuration=JSON.parse(document.getElementById('configuration').value);
-    connection.trigger('updateActivity',configuration);
+connection.on('clickedNext', function () {
+    var configuration = JSON.parse(document.getElementById('configuration').value);
+    connection.trigger('updateActivity', configuration);
 })
