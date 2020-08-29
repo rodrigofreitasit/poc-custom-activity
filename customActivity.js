b8ccc5dd4414cc3078e28db7e87e0a6b6fb7792b
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2020-08-28 23:04:19 -0300

    Resquest Schema on customActivityJS with li just name3

--- customActivity.js
+++ customActivity.js
@@ -23,11 +23,9 @@ connection.on("requestedSchema", function (data) {
   // For to create a LI with values from DE schema
   for (var i = 0; i < data.schema.length; i++) {
     var node = document.createElement("LI"); // Create a <li> node
-    var textnode = document.createTextNode(
-      data.schema[i].key.substring(textnode.lastIndexOf(".") + 1)
-    ); // Create a text node
-    // var li_name = key.substring(textnode.lastIndexOf(".") + 1);
-    node.appendChild(textnode); // Append the text to <li>
+    var textnode = document.createTextNode(data.schema[i].key); // Create a text node
+    var li_name = textnode.substring(textnode.lastIndexOf(".") + 1);
+    node.appendChild(li_name); // Append the text to <li>
     document.getElementById("myList").appendChild(node);
 
     console.log(data.schema[i].key);
