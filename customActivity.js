95ad90b5b84740250add090651a3e61c60a83a31
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2020-08-28 22:45:47 -0300

    Resquest Schema on customActivityJS with li

--- customActivity.js
+++ customActivity.js
@@ -4,11 +4,11 @@ connection.trigger("ready");
 connection.trigger("requestSchema");
 
 connection.on("initActivity", function (data) {
-  document.getElementById("configuration").value = JSON.stringify(
-    data,
-    null,
-    2
-  );
+  //   document.getElementById("configuration").value = JSON.stringify(
+  //     data,
+  //     null,
+  //     2
+  //   );
 });
 
 //save
@@ -22,6 +22,10 @@ connection.on("clickedNext", function () {
 connection.on("requestedSchema", function (data) {
   // save schema
   for (var i = 0; i < data.schema.length; i++) {
+    var node = document.createElement("LI"); // Create a <li> node
+    var textnode = document.createTextNode(data.schema[i].key); // Create a text node
+    node.appendChild(textnode); // Append the text to <li>
+    document.getElementById("myList").appendChild(node);
     console.log(data.schema[i].key);
   }
   console.log("*** Schema ***", JSON.stringify(data["schema"]));
