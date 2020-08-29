6655386c0ebf43c2dd954d8f55661bf042f51049
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2020-08-28 22:04:15 -0300

    Resquest Schema on customActivityJS

--- customActivity.js
+++ customActivity.js
@@ -1,15 +1,26 @@
 var connection = new Postmonger.Session();
 
-connection.trigger('ready');
+connection.trigger("ready");
+connection.trigger("requestSchema");
 
-connection.on('initActivity', function (data) {
-    document.getElementById('configuration').value = JSON.stringify(data, null, 2);
-
+connection.on("initActivity", function (data) {
+  document.getElementById("configuration").value = JSON.stringify(
+    data,
+    null,
+    2
+  );
 });
 
 //save
+connection.on("clickedNext", function () {
+  var configuration = JSON.parse(
+    document.getElementById("configuration").value
+  );
+  connection.trigger("updateActivity", configuration);
+});
 
-connection.on('clickedNext', function () {
-    var configuration = JSON.parse(document.getElementById('configuration').value);
-    connection.trigger('updateActivity', configuration);
-})
+//requestSchema
+connection.on("requestedSchema", function (data) {
+  // save schema
+  console.log("*** Schema ***", JSON.stringify(data["schema"]));
+});
